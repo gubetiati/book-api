4b4961c16b7a2a8252f34bc892d45e2871a50777
gubetiati <gubetiati@gmail.com>
2024-08-06 09:34:36 -0300

    config mongo e modelo

--- src/index.js
+++ src/index.js
@@ -2,13 +2,30 @@ const express = require('express')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+
+const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = 3000
 
 dotenv.config()
 
-const app = express()
+// Conectar ao MongoDB
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        console.log('Conectado ao MongoDB')
+    }).catch((err) => {
+        console.error('Erro ao conectar ao MongoDB ', err)
+    })
 
+const app = express()
 app.use(bodyParser.json())
+
+// Modelo
+const Book = mongoose.model('Book', {
+    titulo: String,
+    autor: String,
+    ano: Number,
+    descricao: String
+})
 
 app.get('/', (req, res) => {
     res.send('Funcionando')
